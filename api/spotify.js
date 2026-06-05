b3420687bcfc01a8ee77373ef8499f5695490a36
HonkBonk <77726792+marcityvr@users.noreply.github.com>
2026-06-05 11:49:36 +0530

    Update spotify.js

--- api/spotify.js
+++ api/spotify.js
@@ -1,40 +1,35 @@
 module.exports = async (req, res) => {
-const CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
-const CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
-const REFRESH_TOKEN = process.env.SPOTIFY_REFRESH_TOKEN;
+  const CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
+  const CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
+  const REFRESH_TOKEN = process.env.SPOTIFY_REFRESH_TOKEN;
 
-try {
-const tokenResponse = await fetch(
-"https://accounts.spotify.com/api/token",
-{
-method: "POST",
-headers: {
-Authorization:
-"Basic " +
-Buffer.from(
-CLIENT_ID + ":" + CLIENT_SECRET
-).toString("base64"),
-"Content-Type":
-"application/x-www-form-urlencoded",
-},
-body: new URLSearchParams({
-grant_type: "refresh_token",
-refresh_token: REFRESH_TOKEN,
-}),
-}
-);
+  try {
+    const response = await fetch(
+      "https://accounts.spotify.com/api/token",
+      {
+        method: "POST",
+        headers: {
+          Authorization:
+            "Basic " +
+            Buffer.from(
+              CLIENT_ID + ":" + CLIENT_SECRET
+            ).toString("base64"),
+          "Content-Type":
+            "application/x-www-form-urlencoded",
+        },
+        body: new URLSearchParams({
+          grant_type: "refresh_token",
+          refresh_token: REFRESH_TOKEN,
+        }),
+      }
+    );
 
-```
-const tokenData = await tokenResponse.json();
+    const data = await response.json();
 
-return res.status(200).json({
-  tokenData,
-});
-```
-
-} catch (error) {
-return res.status(500).json({
-error: error.message,
-});
-}
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({
+      error: error.message,
+    });
+  }
 };
